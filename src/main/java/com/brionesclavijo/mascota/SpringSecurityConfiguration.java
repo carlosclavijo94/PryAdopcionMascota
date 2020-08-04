f7ae1ee072a99384514cce364fda571eb3eb2e6d
carlosclavijo94 <cmclavijo@espe.edu.ec>
2020-08-04 17:16:05 -0500

    Index y Layout

--- src/main/java/com/brionesclavijo/mascota/SpringSecurityConfiguration.java
+++ src/main/java/com/brionesclavijo/mascota/SpringSecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/","/css/**","/fonts/**","/images/**","/js/**").permitAll()
-			.antMatchers("/mascota/**").hasAnyRole("ADMIN")
+			.antMatchers("/mascota/**").hasAnyRole("USER")
 			.antMatchers("/usuario/**").anonymous()
 			.antMatchers("/photos/area**").anonymous()
 			.antMatchers("/materia/**").hasAnyRole("ADMIN")
